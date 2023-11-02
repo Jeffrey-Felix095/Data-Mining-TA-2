e1a0c7ca8720f15599cd6d546820d408862f9122
Jeffrey-Felix095 <74744182+Jeffrey-Felix095@users.noreply.github.com>
2023-11-02 03:29:16 +0000

    update

--- TA_2_G3/models/punto_5.4/Productos_Frecuentes.sql
+++ TA_2_G3/models/punto_5.4/Productos_Frecuentes.sql
@@ -2,3 +2,5 @@
 
 SELECT CP_.* EXCEPT(producto), PG_.*
 FROM {{  ref("Compras-productos")  }} CP_ INNER JOIN {{  ref("Productos_Generales")  }} PG_ ON CP_.producto = PG_.Codigo
+ORDER BY CP_.Frecuencia DESC
+LIMIT 20
