9dfdb327e3ba6ccd205a8ff979516214bcbf141f
Jeffrey-Felix095 <74744182+Jeffrey-Felix095@users.noreply.github.com>
2023-11-03 18:11:04 +0000

    Pequeños ajustes

--- models/punto_5.2/Clientes_Destacados.sql
+++ models/punto_5.2/Clientes_Destacados.sql
@@ -2,3 +2,4 @@
 
 SELECT clientesD_.*, clientes_.*
 FROM {{  ref("ClientesTop")  }} clientesD_ INNER JOIN {{  source("TA_2_G3", "Clientes")  }} clientes_ ON clientesD_.cod_cli = clientes_.C__digo
+ORDER BY num_comprasTotales DESC
